993136bb552b2050ee1f7bc4bd22ffc4c4c4dc79
nanosplit <jacob@mpulsedesign.com>
2018-04-05 07:37:53 -0500

    test crap

---
+++ test/models/district_school_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class DistrictSchoolTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
---
+++ test/models/message_invoice_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class MessageInvoiceTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
